1657cf3b3cb0499a5398f5d93e8c74890d6b3903
bmaha2 <maharjanbipin@gmail.com>
2020-07-15 21:28:17 -0700

    adds header

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  function Header() {
+    return (
+    
+<div className="jumbotron jumbotron-fluid text-info text-center">
+  <div className="container">
+    <h1 className="display-4">Employee Directory</h1>
+  </div>
+</div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
